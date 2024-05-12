daef448860bda321801e4ebc7cb76172e0943654
sivakumarp120 <sivakumar.p120@gmail.com>
2024-05-12 11:46:10 +0530

    changes related to basic math operations

---
+++ A.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class A {
+  int a, b;
+
+  int mul(int a, int b) {
+      return a * b;
+  }
+
+  int add(int a, int b) {
+      return a + b;
+  }
+
+  int sub(int a, int b) {
+      return a - b;
+  }
+
+}
